bf1a73d63d69a822883a728b2465b23abd90fbcb
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2021-02-09 12:31:46 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -7,7 +7,7 @@
 #=================================================
 
 # Add luci-app-ssr-plus
-pushd package/lean
+pushd code/friendlywrt/package
 git clone --depth=1 https://github.com/fw876/helloworld
 popd
 
@@ -67,38 +67,12 @@ svn co https://github.com/pymumu/luci-app-smartdns/trunk ../luci-app-smartdns
 # Add luci-udptools
 git clone --depth=1 https://github.com/zcy85611/openwrt-luci-kcp-udp
 
-rm -rf friendlywrt/feeds/packages/libs/libcap
-svn co https://github.com/openwrt/packages/trunk/libs/libcap friendlywrt/feeds/packages/libs/libcap
-          
-rm -rf friendlywrt/package/feeds/packages/netdata
-svn co https://github.com/openwrt/packages/trunk/admin/netdata friendlywrt/package/feeds/packages/netdata
-rm -rf friendlywrt/feeds/packages/libs/libnatpmp
-svn co https://github.com/coolsnowwolf/packages/trunk/libs/libnatpmp friendlywrt/feeds/packages/libs/libnatpmp
-          
-rm -rf friendlywrt/feeds/packages/lang/luasec
-svn co https://github.com/coolsnowwolf/packages/trunk/lang/luasec friendlywrt/feeds/packages/lang/luasec
-sed -i '/STAMP_BUILT/d' friendlywrt/feeds/packages/utils/runc/Makefile
-sed -i '/STAMP_BUILT/d' friendlywrt/feeds/packages/utils/containerd/Makefile
-          
-sed -i '/upx/d' friendlywrt/package/lean/frp/Makefile
-          
-wget -O friendlywrt/package/kernel/kmod-sched-cake/Makefile https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/kmod-sched-cake-oot/Makefile
-wget -O friendlywrt/feeds/packages/libs/libxml2/Makefile https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libxml2/Makefile
-sed -i "/redirect_https/d" friendlywrt/package/network/services/uhttpd/files/uhttpd.config
-sed -i '/Load Average/i\\t\t<tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%><span>&#8451;</span></td></tr>' friendlywrt/feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i 's/pcdata(boardinfo.system or "?")/"ARMv8"/' friendlywrt/feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
-sed -i 's/services/vpn/g' friendlywrt/package/feeds/luci/luci-app-openvpn/luasrc/controller/openvpn.lua
-sed -i 's/resolv.conf.d\/resolv.conf.auto/resolv.conf.auto/g' friendlywrt/package/lean/luci-app-flowoffload/root/etc/init.d/flowoffload
-          
-sed -i '/done/imkfs.ext4 \/dev\/mmcblk0p2 && mkdir \/mnt\/mmcblk0p2 && mount \/dev\/mmcblk0p2 \/mnt\/mmcblk0p2 && mount \/dev\/mmcblk0p2 \/opt\n' ./friendlywrt/package/base-files/files/root/setup.sh
-# sed -i 's/\/root/\/mnt\/mmcblk0p2/g' ./friendlywrt/package/luci-app-r2sflasher/root/usr/bin/rom_flash
-
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
 popd
 
 # Mod zzz-default-settings
-pushd package/lean/default-settings/files
+pushd code/friendlywrt/package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
 export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y.%m.%d"))/g" zzz-default-settings
@@ -143,7 +117,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 echo -e " Lean's OpenWrt built on "$(date +%Y.%m.%d)"\n -----------------------------------------------------" >> package/base-files/files/etc/banner
 
-pushd package/lean
+pushd code/friendlywrt/package
 rm -rf autocore
 svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/autocore
 popd
